30c0e10302b7b0bdd79e01e75371ab8883d5dbca
abhijitghate <abhijitsghate0605@gmail.com>
2020-07-07 22:28:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function textVisible() {   
+    if( window.innerWidth < 1400 ) {
+        this.querySelectorAll(".top-left-1400")[0].style.opacity="1";
+        this.querySelectorAll("p")[0].style.opacity="1";
+
+    }
+}
+
+function textInvisible() {
+    if( window.innerWidth < 1400 ) {
+        this.querySelectorAll(".top-left-1400")[0].style.opacity="0";
+    }
+}
+
+var el = document.getElementsByClassName("overlay-inner");
+for (let i of el){
+    i.addEventListener("mouseover", textVisible)
+}
+
+for (let i of el){
+    i.addEventListener("mouseout", textInvisible)
+}
+
